5924474a410d8c2012c1cb12eea21e9bd4c0cd2b
Jacó Apolinário <jacoapollinario@gmail.com>
2020-09-10 08:33:36 -0300

    Private Lessons Project

---
+++ src/app/controller/TeachersController.js
@@ -0,0 +1,23 @@
+module.exports = {
+    index(req, res) {
+        return
+    },
+    create(req, res) {
+        return
+    },
+    post(req, res) {
+        return
+    },
+    show(req, res) {
+        return
+    },
+    edit(req, res) {
+        return
+    },
+    put(req, res) {
+        return
+    },
+    delete(req, res) {
+        return
+    }
+} 
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = express.Router()
+const teachers = require('./app/controller/TeachersController')
+
+routes.get('/', function(req, res) {
+    return res.send("OK")
+})
+
+/*=== Teachers Routes ===*/
+routes.get('/teachers', teachers.index)
+routes.get('/teachers/create', teachers.create)
+routes.get('/teachers/:id', teachers.show)
+routes.get('/teachers/:id/edit', teachers.edit)
+
+routes.post('/teachers', teachers.post)
+routes.put('/teachers', teachers.put)
+routes.delete('/teachers', teachers.delete)
+
+module.exports = routes
